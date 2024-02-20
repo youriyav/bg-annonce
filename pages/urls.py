aa7d34c22c5b6cc83b6705b4644f5460a6a6008e
liye <yavouckolye@gmail.com>
2024-02-20 10:45:39 +0100

    last commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from pages.views import home_view, HomePageView, AboutPageView
+
 urlpatterns = [
-    path('admin/',),
+    #path('', home_view, name='index'),
+    path('', HomePageView.as_view(), name='index'),
+    path('about', AboutPageView.as_view(), name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+def home_view(request, *args, **kwargs):
+    return render(request, 'home.html')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
